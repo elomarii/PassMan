91d1223aa44921b79b0b92f0292941319f685000
Ibrahim EL OMARI <ibrahim.el-omari@grenoble-inp.org>
2024-02-26 19:58:16 +0100

    password management view

--- lib/views/password/password_controller.dart
+++ lib/views/password/password_controller.dart
@@ -1,3 +1,50 @@
+import 'package:PassMan/constants/globals.dart';
+import 'package:PassMan/routes/app_routes.dart';
+import 'package:PassMan/utility.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-class PasswordController extends GetxController {}
+class PasswordController extends GetxController {
+  int? passId;
+  TextEditingController platform = TextEditingController();
+  TextEditingController value = TextEditingController();
+  GlobalKey<FormState> form = GlobalKey();
+
+  @override
+  void onInit() {
+    if (Get.arguments != null) {
+      passId = Get.arguments[0];
+      platform.text = Get.arguments[1];
+      value.text = Get.arguments[2];
+    }
+    super.onInit();
+  }
+
+  Future<void> savePassword() async {
+    if (!form.currentState!.validate()) return;
+    String encryption = await encrypt(value.text);
+    if (passId != null) {
+      await passmanDb.update(
+        passwordsTable,
+        {"platform": platform.text.trim(), "value": encryption},
+        where: "id = ?",
+        whereArgs: [passId],
+      );
+    } else {
+      await passmanDb.insert(
+        passwordsTable,
+        {"platform": platform.text.trim(), "value": encryption},
+      );
+    }
+    Get.offAllNamed(AppRoutes.home);
+  }
+
+  Future<void> deletePassword() async {
+    await passmanDb.delete(
+      passwordsTable,
+      where: "id = ?",
+      whereArgs: [passId],
+    );
+    Get.offAllNamed(AppRoutes.home);
+  }
+}
--- lib/views/password/password_view.dart
+++ lib/views/password/password_view.dart
@@ -1,11 +1,62 @@
+import 'package:PassMan/constants/colors.dart';
+import 'package:PassMan/views/password/password_controller.dart';
 import 'package:PassMan/widgets/app_scaffold.dart';
+import 'package:PassMan/widgets/button.dart';
+import 'package:PassMan/widgets/text_field.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-class PasswordView extends StatelessWidget {
+class PasswordView extends GetView<PasswordController> {
   const PasswordView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return AppScaffold(body: const Placeholder());
+    return AppScaffold(
+      bgColor: AppColors.second,
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: AppColors.first,
+        elevation: 2,
+        title: Text(
+          "Add/Edit a password",
+          style: TextStyle(
+            color: AppColors.fifth,
+            fontSize: 19,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: Form(
+        key: controller.form,
+        child: Column(
+          children: [
+            const SizedBox(height: 20),
+            AppTextField(
+              controller: controller.platform,
+              label: "platform",
+              fillColor: AppColors.first,
+            ),
+            const SizedBox(height: 10),
+            AppTextField(
+              controller: controller.value,
+              label: "value",
+              fillColor: AppColors.first,
+            ),
+            const SizedBox(height: 25),
+            Button(
+              title: "save",
+              onPressed: controller.savePassword,
+            ),
+            Visibility(
+              visible: controller.passId != null,
+              child: Button(
+                title: "delete",
+                onPressed: controller.deletePassword,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
