bf5b0a5d08992bea26bc03eebec84813c96c3871
Ibrahim EL OMARI <i.ibrahim.elomari@gmail.com>
2024-03-07 22:07:25 +0100

    adjustment to button

--- lib/widgets/button.dart
+++ lib/widgets/button.dart
@@ -2,10 +2,16 @@ import 'package:PassMan/constants/colors.dart';
 import 'package:flutter/material.dart';
 
 class Button extends StatefulWidget {
-  const Button({super.key, required this.title, required this.onPressed});
+  const Button({
+    super.key,
+    required this.title,
+    required this.onPressed,
+    this.expanded = false,
+  });
 
   final String title;
   final Future Function() onPressed;
+  final bool expanded;
 
   @override
   State<Button> createState() => _ButtonState();
@@ -16,7 +22,7 @@ class _ButtonState extends State<Button> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 100,
+      width: widget.expanded ? double.maxFinite : 100,
       color: AppColors.first,
       alignment: Alignment.center,
       child: InkWell(
@@ -32,7 +38,7 @@ class _ButtonState extends State<Button> {
                 });
               },
         child: Padding(
-          padding: const EdgeInsets.symmetric(vertical: 8),
+          padding: EdgeInsets.symmetric(vertical: widget.expanded ? 12 : 8),
           child: Text(
             processing ? "..." : widget.title,
             style: TextStyle(color: AppColors.fifth, fontSize: 16),
