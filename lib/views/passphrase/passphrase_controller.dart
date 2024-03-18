12af97ba8a94688794f0bb8054aafffe5b0cb43e
Ibrahim EL OMARI <i.ibrahim.elomari@gmail.com>
2024-03-18 03:32:12 +0100

    minor fixes

--- lib/views/passphrase/passphrase_controller.dart
+++ lib/views/passphrase/passphrase_controller.dart
@@ -5,19 +5,22 @@ import 'package:PassMan/constants/globals.dart';
 import 'package:PassMan/routes/app_routes.dart';
 import 'package:PassMan/utility.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 import 'package:hex/hex.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 class PassphraseController extends GetxController {
+  GlobalKey<FormState> formkey = GlobalKey();
   TextEditingController old = TextEditingController();
   TextEditingController neu = TextEditingController();
 
   Future<void> changePassphrase() async {
+    if (!formkey.currentState!.validate()) return;
     String given = HEX.encode(utf8.encode(old.text));
     if (given != passphrase) {
-      showdialog("wrong passphrase !");
+      showdialog("wrong current passphrase !");
       return;
     }
 
--- lib/views/passphrase/passphrase_view.dart
+++ lib/views/passphrase/passphrase_view.dart
@@ -15,30 +15,39 @@ class PassphraseView extends GetView<PassphraseController> {
     return AppScaffold(
       bgColor: AppColors.second,
       appBar: const PassBar(title: "config"),
-      body: Column(
-        children: [
-          const SizedBox(height: 20),
-          AppTextField(
-            controller: controller.old,
-            label: "current passphrase",
-            fillColor: AppColors.first,
-          ),
-          const SizedBox(height: 10),
-          AppTextField(
-            fillColor: AppColors.first,
-            controller: controller.neu,
-            label: "new passphrase",
-          ),
-          const SizedBox(height: 20),
-          Button(title: "save", onPressed: controller.changePassphrase),
-          const SizedBox(height: 20),
-          Divider(color: AppColors.fourth),
-          const SizedBox(height: 20),
-          Button(
-              title: ">> export passwords >>",
+      body: Form(
+        key: controller.formkey,
+        child: Column(
+          children: [
+            const SizedBox(height: 20),
+            AppTextField(
+              controller: controller.old,
+              label: "current passphrase",
+              fillColor: AppColors.first,
+            ),
+            const SizedBox(height: 10),
+            AppTextField(
+              fillColor: AppColors.first,
+              controller: controller.neu,
+              label: "new passphrase",
+              onSubmit: (_) => controller.changePassphrase(),
+            ),
+            const SizedBox(height: 20),
+            Divider(color: AppColors.fourth),
+            const SizedBox(height: 20),
+            Button(
+              title: "<< export passwords >>",
               expanded: true,
-              onPressed: controller.exportData),
-        ],
+              onPressed: controller.exportData,
+            ),
+            const SizedBox(height: 10),
+            Button(
+              title: ">> import passwords <<",
+              expanded: true,
+              onPressed: () async {},
+            ),
+          ],
+        ),
       ),
     );
   }
